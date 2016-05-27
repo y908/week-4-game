20903264670eed2366c50817d1d4bfc986c80c95
Yana Yakubchik <Yana Yakubchik>
2016-05-27 16:30:43 -0400

    homework week4 edit 3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,56 +4,70 @@ window.onload = function () {
 //id gameNum = is the number the computer chose 
 //id yourNum = is the number you currently have
 //id score = the players current score
-//class gem = the button for gems
+//id gemButton = the button for gems
 
 
 var gameNumber; //is the number the comp chose
-var yourNumber; //the number user currently has
+var UserScore = 0; //the number user currently has
 var currentScore; // the players current score 
-var gem; // buttons for gems
+var gemNumber = [12, 3, 45, 5]; // buttons for gems
+var gemImage = ['assets/images/gem1.jpg', 'assets/images/gem2.jpg', 'assets/images/gem3.jpg', 'assets/images/gem4.jpg']; // buttons for gems
 var message; // alerts the user
 
 
 //----When the game starts------
 
+function newGame(){
 
+	for (var i=0; i< gemNumber.length; i++){
+		gemNumber[i] = Math.floor((Math.random() * 22) + 1);
+	}
 
+}
 
-
+newGame();
 
 //----The Game Score-----
-gameNumber = document.getElementById("gameNum");
-gameNumber.innerHTML = Math.floor((Math.random() * 108) + 12);
+gameNumber = Math.floor((Math.random() * 108) + 12);
+$("#gameNum").append(gameNumber);
+
+
 
 
 //--------Value of Gem Buttons/ User Score--------------
 
+
 $(".gem1").on("click", function(){
-					alert("I was clicked.  1");
+					alert("I was clicked.");
+					});
+
+
+// populate gems and display them
+for (var i=0; i< gemNumber.length; i++){
+			var gem2 = $('<img>');
+			gem2.attr('data-num', gemNumber[i]);
+			gem2.attr('src', gemImage[i]);
+			gem2.attr('alt', '');
+
+
+			// for debugging only
+			gem2.attr('title', gemNumber[i]);
+
+
+
+			gem2.attr('height', '100px');
+			gem2.addClass('gemTwo');
+
+			$('#gemButton').append(gem2);
+}
+
+
+
+$('.gemTwo').on('click', function(){
+	UserScore = UserScore + parseInt($(this).data('num'));
+	$('#yourNum').text(UserScore);
 });
+     
 
-$(".gem2").on("click", function(){
-					alert("I was clicked.  2");
-});
-
-
-$(".gem4").on("click", function(){
-        		yourNumber = document.getElementById("yourNum");
-        		yourNumber.innerHTML = Math.floor((Math.random() * 99) + 1);
-
-
-        		if (yourNumber == gameNumber){
-						alert("Whoa dog, you won. How did that happen?");
-						}
-
-						else {
-						alert("You lost. No big deal though, I've only just started this homework. Nothing is really working yet")
-						}
-
-			});
-
-$(".gem3").on("click", function(){
-					alert("I was clicked.  3");
-});
 
 }//closing tag for window.onload
