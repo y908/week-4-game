e640b941b9b8996ede2c89680724902bcb2c3634
Yana Yakubchik <Yana Yakubchik>
2016-05-27 20:41:45 -0400

    homework week4 edit 4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,33 +13,34 @@ var currentScore; // the players current score
 var gemNumber = [12, 3, 45, 5]; // buttons for gems
 var gemImage = ['assets/images/gem1.jpg', 'assets/images/gem2.jpg', 'assets/images/gem3.jpg', 'assets/images/gem4.jpg']; // buttons for gems
 var message; // alerts the user
-
+var wins = 0; //keeps score for game
+var losses = 0; //keeps score for game
 
 //----When the game starts------
 
 function newGame(){
 
+	//----The Game Score-----
+gameNumber = Math.floor((Math.random() * 108) + 12);
+
+// --------change game number-------------
+$("#gameNum").empty().append(gameNumber);
+
+
+ //----------gem number-------------------
 	for (var i=0; i< gemNumber.length; i++){
-		gemNumber[i] = Math.floor((Math.random() * 22) + 1);
+		gemNumber[i] = Math.floor((Math.random() * 9) + 1);
 	}
+
+//-----score---------
+$("#score").empty().append("Wins: " + wins + "<br>Losses: " + losses);
+
+UserScore = 0;
+$("#yourNum").html(UserScore);
 
 }
 
 newGame();
-
-//----The Game Score-----
-gameNumber = Math.floor((Math.random() * 108) + 12);
-$("#gameNum").append(gameNumber);
-
-
-
-
-//--------Value of Gem Buttons/ User Score--------------
-
-
-$(".gem1").on("click", function(){
-					alert("I was clicked.");
-					});
 
 
 // populate gems and display them
@@ -48,26 +49,31 @@ for (var i=0; i< gemNumber.length; i++){
 			gem2.attr('data-num', gemNumber[i]);
 			gem2.attr('src', gemImage[i]);
 			gem2.attr('alt', '');
-
-
-			// for debugging only
-			gem2.attr('title', gemNumber[i]);
-
-
-
-			gem2.attr('height', '100px');
+	   	gem2.attr('height', '100px');
 			gem2.addClass('gemTwo');
 
 			$('#gemButton').append(gem2);
 }
 
-
+//--------------wins and losses--------------
 
 $('.gemTwo').on('click', function(){
 	UserScore = UserScore + parseInt($(this).data('num'));
 	$('#yourNum').text(UserScore);
+
+
+if (UserScore == gameNumber){
+	alert('You won!');
+	wins++;
+	newGame();
+}else if (UserScore > gameNumber){
+	alert('You lost!');
+	losses++;
+	newGame();
+}
+
 });
-     
+
 
 
 }//closing tag for window.onload
